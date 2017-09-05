4919fbcd3f7c3e450ac7e4c52a115d368ca588f1
vishalbhatia09 <vishalbhatia09@mail.fresnostate.edu>
2017-09-05 15:16:02 -0400

    update db connections

--- insert_data.go
+++ insert_data.go
@@ -14,8 +14,8 @@ var db *sql.DB
 var stmtIns *sql.Stmt
 
 func inserting_data(i int, db *sql.DB, stmtIns *sql.Stmt){
-
-	db.SetMaxOpenConns(10)
+	fmt.Println(i)
+	db.SetMaxOpenConns(100)
 	
 	_, err := stmtIns.Exec(i,"owner name", "business name", "site address one", "site address two", "site city", "site state", rand.Intn(250000), "mail address", "mail city", "mail state", rand.Intn(45000), "phone number", "start date", i+200, time.Date(2017, time.September, 05, 0, 0, 0, 0, time.UTC), i+300, "authorized user", "original user", "external source info", "tax payer id", "business lic number", i +10, "export date", i+2 ,"car report date", rand.Intn(1000), time.Now(), rand.Intn(50), "notice 2 date", rand.Intn(60000), "notice 3 date", "billing number", "notice 4 date", "notice 5 date", "billing 1 date", "billing 2 date", "billing 3 date", "billing 4 date", "billing 5 date", "billing 6 date", "billing 7 date", "amount", "emailid" )
 	
@@ -69,8 +69,7 @@ func main() {
 
 	startTime := time.Now()
 	// Insert data in table case inventory
-	for i := 1000; i < 20000; i++ {
-		fmt.Println(i)
+	for i := 1 ; i <= 200001 ; i++ {
 		wg.Add(1)
 		go inserting_data(i, db, stmtIns)		
 	}
