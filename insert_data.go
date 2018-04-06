61c50e92cba65baa06bc06d71991fdc8dba57842
vishalbhatia09 <libra.bhatia@gmail.com>
2018-04-06 10:21:40 -0400

    Update insert_data.go

--- insert_data.go
+++ insert_data.go
@@ -30,7 +30,7 @@ func inserting_data(i int, db *sql.DB, stmtIns *sql.Stmt){
 
 func main() {
 
-	db, err := sql.Open("mysql", "root:password@/test")
+	db, err := sql.Open("mysql", "user:password@/test")
 	if err != nil {
 		panic(err.Error()) // Just for example purpose. You should use proper error handling instead of panic
 	}
